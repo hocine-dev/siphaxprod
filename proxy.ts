239dc7ccac110ced748f658699de76e40f276536
HocineDev <hocinedev4@gmail.com>
2025-11-02 11:50:11 +0100

    un pause

--- proxy.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function proxy(req: NextRequest) {
-  return new NextResponse(
-    "<h1 style='font-family:sans-serif;text-align:center;margin-top:20%'>Site temporarily paused</h1>",
-    { headers: { "content-type": "text/html" } }
-  );
-}
